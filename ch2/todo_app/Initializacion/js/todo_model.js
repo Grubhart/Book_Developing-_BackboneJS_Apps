11bab8f8d2a7d0fc60e3575805a62369cec40f7d
grubhart <grubhart@gmail.com>
2014-08-29 10:17:06 -0500

    demo

---
+++ ch2/todo_app/Initializacion/js/todo_model.js
@@ -0,0 +1,7 @@
+var Todo = Backbone.Model.extend({
+    initialize: function(){
+        console.log('This model has been initialized.');
+    }
+});
+var myTodo = new Todo();
+// Logs: This model has been initialized.
