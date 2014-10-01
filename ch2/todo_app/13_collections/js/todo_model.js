2169e660350161a8864b7ac978206d0af3eec60e
grubhart <grubhart@gmail.com>
2014-10-01 18:21:14 -0500

    added code to collections

---
+++ ch2/todo_app/13_collections/js/todo_model.js
@@ -0,0 +1,20 @@
+var Todo = Backbone.Model.extend({
+    defaults : {
+        title : '',
+        completed : false
+    }
+});
+
+var TodoCollection = Backbone.Collection.extend({
+    model : Todo
+})
+
+
+var myTodo = new Todo({ title:'Read the whole book' , id:2});
+
+//pass array of models on collection instantiation
+
+var todos = new TodoCollection([myTodo]);
+
+console.log("Collection size : " + todos.length); // Collection size : 1
+
